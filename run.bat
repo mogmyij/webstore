1771a025aa7859672b8723d77833bfccea17164c
mogmyij <foonglucas@gmail.com>
2025-08-17 15:41:24 +0800

    fix batch file

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 title Karvana Launcher
 
 REM This starts the server script in a new window and waits for it to close.
-start "Karvana Server" /wait cmd /c start-server.bat
+start "Karvana Server" /wait cmd /c launch-admin.bat
 
 REM --- The code below will only run AFTER the server window is closed ---
 
